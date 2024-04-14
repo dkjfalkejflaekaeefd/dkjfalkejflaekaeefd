78d31875a39bef7a6263ac1f7a0184b80ccdaf92
설소연 <seolsoyeon@seolsoyeon-ui-MacBookAir.local>
2024-04-14 16:42:10 +0900

    Initial commit

---
+++ pages/posts/first-post.js
@@ -0,0 +1,15 @@
+import Link from 'next/link';
+
+export default function FirstPost() {
+  return (
+    <>
+      <h1>hi!!!! i'm soyeon. 
+        my hobby is playing a guitar.
+        because i feel comfortable when i listen to my guitar playing 
+        thank you. have a nice day!!!</h1>
+      <h2>
+        <Link href="/">Back to home</Link>
+      </h2>
+    </>
+  );
+}
